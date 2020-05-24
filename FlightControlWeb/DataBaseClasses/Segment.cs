4f5383cb762ede3f4d124d66926fde8bcec0e87a
zivza94 <zivza94@gmail.com>
2020-05-24 14:52:46 +0300

    add Segment

---
+++ FlightControlWeb/DataBaseClasses/Segment.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlightControlWeb.DataBaseClasses
+{
+    public class Segment
+    {
+        public Segment()
+        { }
+        public Segment(double startLatitude, double startLongitude, int timespan)
+        {
+            Latitude = startLatitude;
+            Longitude = startLongitude;
+            TimespanSeconds = timespan;
+        }
+
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public int TimespanSeconds { get; set; }
+    }
+}
